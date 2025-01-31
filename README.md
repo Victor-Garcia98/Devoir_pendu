ebc83817353317c3c72205ed7e6a82a6cd5e9bf4
VictorGarcia <victor.garcia.1@ens.etsmtl.ca>
2025-01-30 21:26:59 -0500

    Push du pendu, du readme et du fichier texte par défaut pour le jeu

---
+++ README.md
@@ -0,0 +1,15 @@
+Exécuter le script lance une partie du jeu du pendu avec 6 vies pour l'utilisateur.
+
+le code fonctionne autour de 5 fonctions : 
+- la fonction pendu() réalise le jeu et ses fonctionnalités de base 
+- la fonction charger_mots(), charge les mots depuis le fichiers 
+- la fonction regles() affiche les règles à l'utilisateur 
+- la fonction retirer_accents() utilise un dictionnaire des accents courants pour normaliser
+  les lettres employées
+- la fonction rejouer() permet à l'utilisateur de relancer une partie si celui ci le désire
+
+L'option d'intégrer sa propre liste de mot au format txt est disponible. 
+L'option de normalisation des lettres avec les accents est disponible. 
+
+Potentiel ajout qu'il reste à réaliser :
+- l'aide à l'utilisateur lorsque celui ci n'a plus qu'une seule vie.
---
+++ main.py
@@ -0,0 +1,77 @@
+import random
+
+accents = {
+    'à': 'a', 'â': 'a', 'ä': 'a', 'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+    'î': 'i', 'ï': 'i', 'ô': 'o', 'ö': 'o', 'ù': 'u', 'û': 'u', 'ü': 'u',
+    'ç': 'c',
+}
+def regles():
+    print("les règles du jeu sont les suivantes :\n\n"
+          "vous devez trouver le mot choisi aléatoirement dans le fichier lettre par lettre\n"
+          "A chaque tour, vous pourrez choisir une lettre à essayer\n"
+          "Si celle ci est dans le mot, en une ou plusieurs fois, l'affichage s'actualisera et un nouveau tour se lance\n"
+          "Si celle ci n'est pas dans le mot, vous perdrez une vie\n"
+          "Vous avez 6 vies\n"
+          "Bonne chance \n\n" )
+
+def retirer_accents(mots):
+    return ''.join(accents.get(lettre, lettre) for lettre in mots)
+
+def charger_mots(fichier):
+    with open(fichier, "r", encoding="utf-8") as fichier:
+        mots = [retirer_accents(line.strip().lower()) for line in fichier]
+    return mots
+
+def rejouer():
+    choix = input("voulez vous rejouer ? (oui/non) \n")
+    if choix ==  'oui' :
+        pendu()
+    else :
+        print("\nau revoir !")
+
+
+def pendu():
+    regles()
+    demande_fichier=input('Avez vous un fichier de mots avec lequel jouer ? (oui/non) \n')
+    if demande_fichier == 'oui':
+        fichier = input ("insérer le nom complet du fichier avec l'extension txt \n")
+    else :
+        fichier = 'mots_pendu.txt'
+    mots = charger_mots(fichier)
+    mot_choisi = random.choice(mots)
+    bonnes_lettres = []
+    mauvaises_lettres= []
+    vies = 6
+
+    while vies > 0:
+        affichage = "".join([lettre if lettre in bonnes_lettres else "_" for lettre in mot_choisi])
+        print(f"\nMot à deviner: {affichage}")
+        print(f"vies restantes: {vies}")
+        print(f"Lettres non présentes: {mauvaises_lettres} et lettres trouvées: {bonnes_lettres}")
+
+        if affichage == mot_choisi:
+            print("\nVictoire !\n")
+            rejouer()
+            break
+
+        essai = input("Choix du joueur :  ")
+
+        if len(essai) > 1:
+            print("veuillez ne choisir qu'une seule lettre")
+            continue
+
+        if essai in mot_choisi :
+            print("\nbonne pioche! \n")
+            bonnes_lettres.append(essai)
+        else :
+            vies = vies - 1
+            print("\nmauvaise pioche, vous perdez une vie! \n")
+            mauvaises_lettres.append(essai)
+
+    if vies == 0 :
+        print(f"\nVous n'avez plus de vies restantes, c'est perdu \nLe bon mot était {mot_choisi}\n")
+
+        rejouer()
+
+
+pendu()
